6f2ad0b6a5e94038ed894ca1a707e4fe65e484cc
miclko <michael.konopik@ftf.lth.se>
2023-05-30 13:43:02 +0200

    Added is_even function

---
+++ is_even.py
@@ -0,0 +1,14 @@
+#is even function
+
+
+def is_even(x):
+    """Function for checking evenness of integers
+    Input: x integer
+    output:
+    true if x  even
+    false if x odd
+    raise exception if not an integer 
+    """    
+
+
+assert is_even.__doc__ 
